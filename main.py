7245ffc4a28497823e27863dc3cde4e5b72fac33
5hafayet <shafayetahmad1@gmail.com>
2023-04-02 03:16:34 +0530

    enemy introduced

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import pygame
+import random
 
 # initializing the pygame
 pygame.init()
 
 # Title and Icon
 pygame.display.set_caption("SpaceInvader")
-icon = pygame.image.load('Icon/ufo.png')
+icon = pygame.image.load('Icons/ufo.png')
 pygame.display.set_icon(icon)
 
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
 # Player
-playerImg = pygame.image.load('Icon/player_ufo.png')
+playerImg = pygame.image.load('Icons/player_ufo.png')
 playerX = 370
 playerY = 480
-playerX_change = 0;
+playerX_change = 0
+
+# Enemy
+enemyImg = pygame.image.load('Icons/enemy.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.3
+enemyY_change = 40
 
 
 def player(x, y):
     screen.blit(playerImg, (playerX, playerY))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (enemyX, enemyY))
 
 
 # Game loop
@@ -29,6 +41,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        # player position change based on key pressed
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 playerX_change = -0.3
@@ -36,7 +50,28 @@ while running:
                 playerX_change = 0.3
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0;
+                playerX_change = 0
     playerX += playerX_change
+
+    # code to restrict player in the game frame
+    if playerX <= 0:
+        playerX = 0
+    if playerX >= 736:
+        playerX = 736
+
+    # enemy movement
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    if enemyX > 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
+    # Game over logic
+    # if enemyY >= playerY-50:
+    #     if enemyX > playerX-50 or enemyX < playerX + 50:
+    #         print("Game Over")
+
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
